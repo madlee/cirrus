152af029ee3a0c335292dab6904c5fa155d4e51d
Madlee <madlee@sohu.com>
2020-03-27 17:00:20 +0800

    fix bugs

--- cirrus/settings.py
+++ cirrus/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/10",
+        "LOCATION": "redis://127.0.0.1:36379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -94,7 +94,7 @@ CACHES = {
 
     "cirrus": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/11",
+        "LOCATION": "redis://127.0.0.1:36379/3",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
